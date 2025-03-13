87ab40f833b6777bed5b20020560533633a544ec
Amr Morsy <amr.mors@gmail.com>
2025-03-13 04:45:03 -0400

    layout adjustments

--- src/components/GeolocationButton.tsx
+++ src/components/GeolocationButton.tsx
@@ -1,18 +1,15 @@
-import { useTheme } from "../context/ThemeContext";
-
 interface GeolocationButtonProps {
   onLocate: () => void;
 }
 
 const GeolocationButton: React.FC<GeolocationButtonProps> = ({ onLocate }) => {
-  const { theme } = useTheme();
   return (
     <div className="geo-location-btn" role="button" tabIndex={0} onClick={onLocate}>
       <svg xmlns="http://www.w3.org/2000/svg"
-        width="30"
-        height="30"
+        width="20"
+        height="20"
         viewBox="0 0 256 256"
-        fill={theme == "dark" ? "#FBBF24" : "gray"}
+        fill="#495561"
         stroke="#FBBF24">
         <path d="M141.1 243.4 246 12.6 10 117.5h131.1v125.9z" />
       </svg>
